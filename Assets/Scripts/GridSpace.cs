0b6582b089207d56ef602b6f84bcc1b1bc5fa684
dpr5 <meshie75@gmail.com>
2018-05-30 20:00:09 -0400

    Intitial Commit
    Working full game

---
+++ Assets/Scripts/GridSpace.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//created by Dharmesh Rana for Dream in Reality 
+
+public class GridSpace : MonoBehaviour {
+
+    public Button button;
+    public Text buttonText;
+    public string playerSide;
+
+    private GameControl gameControl;
+    
+    void Start () 
+	{
+		
+	}
+	
+	
+	void Update () 
+	{
+		
+	}
+
+    public void SetSpace()
+    {
+        buttonText.text = gameControl.GetPlayerSide();
+        button.interactable = false;
+        gameControl.EndTurn();
+    }
+
+    public void SetGameControllerReference(GameControl controller)
+    {
+        gameControl = controller;
+    }
+}
---
+++ Assets/Scripts/control.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//created by Dharmesh Rana for Dream in Reality 
+
+public class control : MonoBehaviour {
+
+	
+	void Start () 
+	{
+		
+	}
+	
+	
+	void Update () 
+	{
+		
+	}
+}
